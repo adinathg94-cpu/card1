ee67b38cb976b8deb5d5430dc46e9feb52993a68
adinathg94-cpu <adinathg94@gmail.com>
2026-03-28 21:32:00 +0530

    Update next.config.js for hostinger

--- next.config.js
+++ next.config.js
@@ -2,13 +2,17 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const config = require("./src/config/config.json");
 
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   basePath: config.base_path !== "/" ? config.base_path : "",
   trailingSlash: config.site.trailing_slash,
   output: "standalone",
+
+  images: {
+    // Allow images served from your Hostinger domain
+    domains: ["darkviolet-seal-509592.hostingersite.com"],
+  },
 
   webpack: (config) => {
     return config;
